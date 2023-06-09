3f270fc9b584753938735f8f51246d13570a472c
IsuruUshanBandara <119607844+IsuruUshanBandara@users.noreply.github.com>
2023-06-09 23:49:33 +0530

    Add files via upload

---
+++ task1/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertValues objconvertValues = new ConvertValues();
+            objconvertValues.kilometerToMmter();
+            Console.ReadKey();
+        }
+    }
+}
