0d2d46e060cc70c3915ac553b9516f5ab6405cf6
IsuruUshanBandara <119607844+IsuruUshanBandara@users.noreply.github.com>
2023-06-09 23:54:32 +0530

    Add files via upload

---
+++ task2/task2/FindValues.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class FindValues
+    {
+        public double findarea(double radius)
+        {
+                       
+            
+           return (double)Math.PI*(radius*radius);
+      
+
+        }
+        public double findCircumference(double radius)
+        {
+              
+            return 2 * (double)Math.PI * radius;
+
+        }
+    }
+}
---
+++ task2/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius");
+            double radius=double.Parse(Console.ReadLine());
+            FindValues objfindvalues = new FindValues();
+
+            double area = objfindvalues.findarea(radius);
+            Console.WriteLine("area =" + area);
+           double circumference = objfindvalues.findCircumference(radius);
+            Console.WriteLine("Circumference =" + circumference);
+            Console.ReadLine();
+        }
+    }
+}
