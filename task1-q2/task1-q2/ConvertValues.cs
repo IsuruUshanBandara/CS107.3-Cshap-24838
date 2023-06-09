4e17d4176e6c32fe23fc17c3781686993a7866a2
IsuruUshanBandara <119607844+IsuruUshanBandara@users.noreply.github.com>
2023-06-09 23:52:03 +0530

    Add files via upload

---
+++ task1-q2/task1-q2/ConvertValues.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_q2
+{
+    internal class ConvertValues
+    {
+        public void kilometerToMmter(int kilometers)
+        {
+            int meters;
+
+            
+
+            meters = kilometers * 1000;
+
+            Console.WriteLine("Converted meter is: " + meters);
+        }
+    }
+}
---
+++ task1-q2/task1-q2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the kilometer amount");
+            int kilometers = int.Parse(Console.ReadLine());
+            ConvertValues objconvertValues = new ConvertValues();
+            objconvertValues.kilometerToMmter(kilometers);
+            Console.ReadKey();
+        }
+    }
+}
