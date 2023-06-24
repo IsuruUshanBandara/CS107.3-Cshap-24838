252a9745bd910cb60a80852618bb24b2f694033e
IsuruUshanBandara <119607844+IsuruUshanBandara@users.noreply.github.com>
2023-06-24 08:40:24 +0530

    Add files via upload

---
+++ c#_q6/Lab6_q6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_q6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            nclass cls = new nclass();
+            {
+                Console.WriteLine("enter length");
+                int an = Convert.ToInt32(Console.ReadLine());
+                cls.array(an);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ c#_q6/Lab6_q6/nclass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_q6
+{
+    internal class nclass
+    {
+        
+        
+            public void array(int n)
+            {
+                int[] a = new int[n];
+                for(int i=0;i<a.Length;i=i+2)
+                {
+                    Console.WriteLine($"enter your number {i}: ");
+                    a[i]=Convert.ToInt32(Console.ReadLine());
+                    if (i+1<n)
+                    {
+                        a[i + 1] = 0;
+                    }
+
+
+                }
+                for(int f=0;f<a.Length;f++)
+                {
+                    Console.Write($"{a[f]},");
+                }
+             
+
+            }
+         
+    }
+}
